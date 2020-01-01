d2d9127b9e533318135ea2ba270518a1a658d565
Taylor Allen <28820818+allenbyus@users.noreply.github.com>
2020-01-01 16:53:41 +0800

    Delete install-tcping-2.sh

--- install-tcping-2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Debian/Ubuntu
-apt-get install tcptraceroute
-#centos/rehl
-# yum install tcptraceroute
-cd /usr/bin
-sudo wget http://www.vdberg.org/~richard/tcpping
-sudo chmod 755 tcpping
