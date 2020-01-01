f69b8d2ec862e9d8e6c86d906cbeeed8047dafdb
Taylor Allen <28820818+allenbyus@users.noreply.github.com>
2020-01-01 16:53:18 +0800

    Create install-tcping-2.sh

---
+++ install-tcping-2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Debian/Ubuntu
+apt-get install tcptraceroute
+#centos/rehl
+# yum install tcptraceroute
+cd /usr/bin
+sudo wget http://www.vdberg.org/~richard/tcpping
+sudo chmod 755 tcpping
