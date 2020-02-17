c4071731585958ce8287fd057cc0b6c5f6e2283a
Taylor Allen <28820818+allenbyus@users.noreply.github.com>
2020-02-17 17:40:21 +0800

    Update ddns_vpn_updater.sh

--- TinyTUN/ddns_vpn_updater.sh
+++ TinyTUN/ddns_vpn_updater.sh
@@ -4,19 +4,19 @@ doUpdate(){
 	VPN=`tinytun -k mypassword123 -c ${1}:10001`
 	ifconfig $VPN 10.1.0.2 netmask 255.255.255.0
 	iptables -A POSTROUTING -t nat -s 10.1.0.0/24 -j MASQUERADE -o eth0
-	ip route add you_login_ip/32 via 156.96.105.1 dev eth0
-	ip route add ${1}/32 via 156.96.105.1 dev eth0
-	ip route del ${2}/32 via 156.96.105.1 dev eth0
+	ip route add 59.120.227.144/32 via 156.96.105.1 dev eth0
+	ip route add ${1} via 156.96.105.1 dev eth0
+	ip route del ${2} via 156.96.105.1 dev eth0
 	ip route del default
 	ip route add default via 10.1.0.1 dev $VPN
 	bash /etc/v/cidr-cn.sh
 }
 network_check=$(curl -s -w %{http_code} ip.sb)
-if [ "${network_check}" = "000" ];then
+if [[ "${network_check}" == "000" ]];then
 	echo "网络连接：fail"
 	pkill tinytun
 	ip route del default
-	ip route add default via you_local_gateway dev eth0
+	ip route add default via 156.96.105.1 dev eth0
 else
 	echo "网络连接：OK"
 fi
