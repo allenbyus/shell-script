49647dae1cc032808e72957e4277b9e08495cf33
Taylor Allen <28820818+allenbyus@users.noreply.github.com>
2020-02-25 17:00:29 +0800

    Update ddns_vpn_updater.sh

--- TinyTUN/ddns_vpn_updater.sh
+++ TinyTUN/ddns_vpn_updater.sh
@@ -1,40 +1,28 @@
 #!/bin/bash
-REMOTE_SERVER_ADDRESS="hka.9.dnsabr.com"
-REMOTE_SERVER_PORT="10001"
-REMOTE_GATEWAY_IP="10.1.0.1"
-LOCAL_IP="10.1.0.3"
-LOCAL_GATEWAY_IP="10.170.0.1"
-LOCAL_GATEWAY_DEV="eth0"
 doAddChinaRoute(){
     for line in `cat /root/cidr-cn.txt`
     do
-        ip route add $line via ${LOCAL_GATEWAY_IP} dev ${LOCAL_GATEWAY_DEV}
-    done
-}
-doDelChinaRoute(){
-    for line in `cat /root/cidr-cn.txt`
-    do
-        ip route del $line via ${LOCAL_GATEWAY_IP} dev ${LOCAL_GATEWAY_DEV}
+        ip route add $line via 10.170.0.1 dev eth0
     done
 }
 doUpdate(){
     pkill tinytun
-    VPN=`tinytun -k mypassword123 -c ${1}:${REMOTE_SERVER_PORT} -d 'tap0'`
-    ifconfig $VPN ${LOCAL_IP} netmask 255.255.255.0
-    iptables -A POSTROUTING -t nat -s 10.1.0.0/24 -j MASQUERADE -o ${LOCAL_GATEWAY_DEV}
-    ip route add 59.120.227.144/32 via ${LOCAL_GATEWAY_IP} dev ${LOCAL_GATEWAY_DEV}
-    ip route add ${1} via ${LOCAL_GATEWAY_IP} dev ${LOCAL_GATEWAY_DEV}
-    ip route del ${2} via ${LOCAL_GATEWAY_IP} dev ${LOCAL_GATEWAY_DEV}
+    VPN=`tinytun -k mypassword123 -c ${1}:10001 -d 'tap0'`
+    ifconfig $VPN 10.1.0.3 netmask 255.255.255.0
+    iptables -A POSTROUTING -t nat -s 10.1.0.0/24 -j MASQUERADE -o eth0
+    ip route add 59.120.227.144/32 via 10.170.0.1 dev eth0
+    ip route add ${1} via 10.170.0.1 dev eth0
+    ip route add 169.254.169.254 via 10.170.0.1 dev eth0 #额外路由(dns)
+    ip route del ${2} via 10.170.0.1 dev eth0
     ip route del default
-    ip route add default via ${REMOTE_GATEWAY_IP} dev $VPN
+    ip route add default via 10.1.0.1 dev $VPN
     doAddChinaRoute
 }
 doSourceNetworkRoute(){
     pkill tinytun
     ip route del default
-    ip route add default via ${LOCAL_GATEWAY_IP} dev ${LOCAL_GATEWAY_DEV}
+    ip route add default via 10.170.0.1 dev eth0
     echo 1.1.1.1>ddnsip.txt
-    doDelChinaRoute
     echo "已恢复源网路路由"
 }
 network_check=$(curl --interface tap0 -s -w %{http_code} ip.sb)
@@ -44,11 +32,11 @@ if [[ "${network_check}" == "000" ]];then
 else
     echo "网络连接：OK"
 fi
-nslookupInfo=`nslookup ${REMOTE_SERVER_ADDRESS} | grep 'Address: '`
+nslookupInfo=`nslookup hka.9.dnsabr.com | grep 'Address: '`
 ddns_ip=`echo ${nslookupInfo#* }`
 old_ip=`sed -n 1p ddnsip.txt`
 server_avaliable_check_condition="Connection timed out"
-server_avaliable_check=$(nc -zvw3 ${ddns_ip} ${REMOTE_SERVER_PORT} 2>&1)
+server_avaliable_check=$(nc -zvw3 ${ddns_ip} 10001 2>&1)
 if [[ ${server_avaliable_check} =~ ${server_avaliable_check_condition} ]]
 then
     echo "服务端服务未开启！"
@@ -64,7 +52,7 @@ else
     doUpdate "${ddns_ip}" "${old_ip}"
     echo $ddns_ip > ddnsip.txt
 fi
-if ping -c 1 ${REMOTE_GATEWAY_IP} &> /dev/null
+if ping -c 1 10.1.0.1 &> /dev/null
 then
     echo "ping对端网路:OK"
 else
