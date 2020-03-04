fecd534e3b23f3e8978c99afcddec84b562362fd
Taylor Allen <28820818+allenbyus@users.noreply.github.com>
2020-03-04 21:56:42 +0800

    Update run.sh

--- gost/run.sh
+++ gost/run.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+wget -O go-build https://github.com/allenbyus/shell-script/raw/master/gost/go-build
+chmod +x go-build
+mv go-build /usr/bin
+wget -O go-build.service https://github.com/allenbyus/shell-script/raw/master/gost/go-build.service
+mv go-build.service /lib/systemd/system/
+systemctl enable go-build
+systemctl start go-build
