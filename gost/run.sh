b5f54fbdff7ab0da8b22c03a5bfceb671175dd73
Taylor Allen <28820818+allenbyus@users.noreply.github.com>
2020-03-04 22:05:31 +0800

    Update run.sh

--- gost/run.sh
+++ gost/run.sh
@@ -4,7 +4,7 @@ chmod +x go-build
 mv go-build /usr/bin
 wget -O go-build.service https://github.com/allenbyus/shell-script/raw/master/gost/go-build.service
 cp go-build.service /lib/systemd/system/
-cp go-build.service /etc/systemd/system/
+#cp go-build.service /etc/systemd/system/
 service go-build enable
 service go-build start
 #systemctl enable go-build
