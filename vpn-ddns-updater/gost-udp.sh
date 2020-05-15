3476929e122043dd0b93bc47f2b700d459dddc91
Taylor Allen <28820818+allenbyus@users.noreply.github.com>
2020-05-15 08:29:39 +0800

    fix some bug

--- vpn-ddns-updater/gost-udp.sh
+++ vpn-ddns-updater/gost-udp.sh
@@ -1,60 +1,82 @@
 #!/bin/bash
-VPSNAME="HKT"
+LOG=""
+VPSNAME="HK"
+VPSINTERFACE="eth0"
+VPSIP="12.2.2.2"
+VPSGATEWAY="12.2.2.1"
+VPSVPNIP="192.12.12.2"
+VPSVPNGATEWAY="192.12.12.1"
+VPN_SERVER_DDNS="ddns.vpn.server"
+REPORT_API_URL="https://report_api_url/?send?auth=****"
 #echo "212 cmi" >> /etc/iproute2/rt_tables
 ip route flush table cmi
-ip route add default via 10.170.0.1 dev eth0 src 10.170.0.5 table cmi
-ip rule add from 10.170.0.5 table cmi
+ip route add default via ${VPSGATEWAY} dev ${VPSINTERFACE} src ${VPSIP} table cmi
+ip rule add from ${VPSIP} table cmi
+doKillProcess() {
+    pid=$(ps -ef | grep 'go -L tun://:9222/' | grep -v grep | awk '{print $2}')
+    if [[ "${pid}" == "" ]];then
+        echo "first boot success."
+    else
+        kill ${pid}
+        LOG+="${VPSNAME}: kill process success."
+        doReport
+    fi
+}
 doUpdate() {
-	pkill go
-	nohup go -L tun://:9222/${1}:10001?net=192.168.123.4/24 >/dev/null &
-	sleep 3s
-	ip route add ${1} via 10.170.0.1 dev eth0
-	ip route del default
-	ip route add default via 192.168.123.1
+    doKillProcess
+    nohup go -L tun://:9222/${1}:10001?net=${VPSVPNIP}/24&name=tun0 >/dev/null &
+    sleep 3s
+    ip route add ${1} via ${VPSGATEWAY} dev ${VPSINTERFACE}
+    ip route del default
+    ip route add default via ${VPSVPNGATEWAY}
 }
 doSourceNetworkRoute() {
-	pkill go
-	ip route del default
-	ip route add default via 10.170.0.1 dev eth0
-	echo $ddns_ip > /etc/v/ddnsip.txt
+    doKillProcess
+    ip route del default
+    ip route add default via ${VPSGATEWAY} dev ${VPSINTERFACE}
+    echo $ddns_ip > /etc/v/ddnsip.txt
 }
 doReport() {
-	curl -d ${1} "https://tgpush.fjj.us/send?chatid=2*0****6&sign=*******"
+    curl -d "${LOG}" "${REPORT_API_URL}"
+    LOG=""
 }
 network_check=$(curl --interface tun0 -s -w %{http_code} ip.sb)
 if [[ "${network_check}" == "000" ]];then
-    echo "网络连接：fail"
-    doReport "${VPSNAME}: curl网络连接测试失败"
+    echo "Internet connection:fail"
+    LOG+="${VPSNAME}: curl network connection test failed"
+    doReport
     doSourceNetworkRoute
 else
-    echo "网络连接：OK"
+    echo "Internet connection: OK"
 fi
-nslookupInfo=`nslookup YOUR_SERVER_DDNS | grep 'Address: '`
+nslookupInfo=`nslookup ${VPN_SERVER_DDNS} | grep 'Address: '`
 ddns_ip=`echo ${nslookupInfo#* }`
 old_ip=`sed -n 1p /etc/v/ddnsip.txt`
 #server_avaliable_check_condition="Connection timed out"
 #server_avaliable_check=$(nc -zvw3 ${ddns_ip} 11110 2>&1)
 #if [[ ${server_avaliable_check} =~ ${server_avaliable_check_condition} ]]
 #then
-#    echo "服务端服务未开启！"
+#    echo "The server service is not enabled!"
 #    exit
 #else
-#    echo "服务端服务已开启！"
+#    echo "The server service has been started!"
 #fi
-echo "记录中的地址：${old_ip}!"
+echo "Address in record:${old_ip}!"
 if [ "${old_ip}" = "${ddns_ip}" ];then
-    echo "有效，IP：${ddns_ip}"
+    echo "Effective, IP:${ddns_ip}"
 else
-    echo "过期，新IP：${ddns_ip}!"
-    doReport "${VPSNAME}: IP${old_ip}过期，新IP：${ddns_ip}!"
+    echo "Expired, new IP:${ddns_ip}!"
+    LOG+="${VPSNAME}: IP${old_ip} Expired, new IP: ${ddns_ip}!"
+    doReport
     doUpdate "${ddns_ip}" "${old_ip}"
     echo $ddns_ip > /etc/v/ddnsip.txt
 fi
-if ping -c 1 192.168.123.1 &> /dev/null
+if ping -c 1 ${VPSVPNGATEWAY} &> /dev/null
 then
-    echo "ping对端网路:OK"
+    echo "Ping the peer network: OK"
 else
-    echo "ping对端网路:fail"
-    doReport "${VPSNAME}: ping对端网路测试失败"
+    echo "Ping the peer network: fail"
+    LOG+="${VPSNAME}: Ping the peer network: fail"
+    doReport
     doSourceNetworkRoute
 fi
