fa666b94ede07b912df3be2fed823364d06cbfee
ifeng <16521693+fengjijiao@users.noreply.github.com>
2020-12-22 19:37:29 +0800

    Update backup-image.sh

--- dd/backup-image.sh
+++ dd/backup-image.sh
@@ -2,5 +2,9 @@
 fdisk -u -l /dev/vda
 #有压缩
 dd bs=512 count=14680063 if=/dev/vda | gzip -6 > ghost.img.gz
+#还原
+gzip -dc ghost.img.gz | dd of=/dev/vda
 #无压缩
 dd bs=512 count=14680063 if=/dev/vda of=ghost.img
+#还原
+dd if=ghost.img of=/dev/vda
